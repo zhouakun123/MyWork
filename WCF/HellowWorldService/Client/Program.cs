9386de3b7e2202199ab7b5cd88f046434f8d5e74
zhouakun123 <553467321@qq.com>
2017-02-23 15:25:16 +0800

    Add HelloWorldService

---
+++ WCF/HellowWorldService/Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Client.HelloWorldServiceReference;
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorldClient client = new HelloWorldClient("NetTcpBinding_IHelloWorld");
+            Name person = new Name();
+            person.First = "Howard";
+            person.Last = "Zhou";
+            Console.WriteLine(client.SayHello(person));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WCF/HellowWorldService/HellowWorldService/HelloWorld.cs
@@ -0,0 +1,64 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="HelloWorld.cs" company="Hx">
+//   CopyRight by Hx
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace HellowWorldService
+{
+    #region Using
+
+    using System.Runtime.Serialization;
+    using System.ServiceModel;
+
+    #endregion
+
+    /// <summary>The name.</summary>
+    [DataContract]
+    public class Name
+    {
+        #region Fields
+
+        /// <summary>The first.</summary>
+        [DataMember]
+        public string First;
+
+        /// <summary>The last.</summary>
+        [DataMember]
+        public string Last;
+
+        #endregion
+    }
+
+    /// <summary>The HelloWorld interface.</summary>
+    [ServiceContract]
+    public interface IHelloWorld
+    {
+        #region Public methods and operators
+
+        /// <summary>The say hello.</summary>
+        /// <param name="persoName">The perso name.</param>
+        /// <returns>The <see cref="string"/>.</returns>
+        [OperationContract]
+        string SayHello(Name persoName);
+
+        #endregion
+    }
+
+    /// <summary>The hello world service.</summary>
+    public class HelloWorldService : IHelloWorld
+    {
+        #region Interface methods
+
+        /// <summary>The say hello.</summary>
+        /// <param name="persoName">The perso name.</param>
+        /// <returns>The <see cref="string"/>.</returns>
+        public string SayHello(Name persoName)
+        {
+            return string.Format("Hello {0} {1}", persoName.First, persoName.Last);
+        }
+
+        #endregion
+    }
+}
